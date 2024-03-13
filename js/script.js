9080daf083a161b98edd85b26ffdb83023a559d6
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-13 15:04:57 +0100

    [add] Milestone 3

--- js/script.js
+++ js/script.js
@@ -9,14 +9,19 @@ createApp({
         { nome: "Assicurare il processore all'intercapedine C", fatto: false },
         { nome: "Inserire il gancio B nell'apposito alloggiamento", fatto: false },
         { nome: "Agganciare il calcio facendo pressione sulla base del medesimo", fatto: false},
-        { nome: "Infilare nel micro post processore la canna del mirino che si basa sull'ordine della grandezza del medesimo ", fatto: false}
+        { nome: "Infilare nel micro post processore la canna del mirino che si basa sull'ordine della grandezza del medesimo ", fatto: false},
       ],
       error:'',
+      newTask: ''
     }
   },
   methods:{
+    addTask(){
+      this.toDoList.unshift({ nome: `${this.newTask}`, fatto: false},);
+      this.newTask= ''
+    },
     deleteTask(index){
-        this.toDoList.splice(index, 1)
+        this.toDoList.splice(index, 1);
     }
   }
 }).mount('#app');
